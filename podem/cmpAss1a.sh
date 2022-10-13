c503fa1c8b7a2d8e011cd28d5c83ada228b22560
esfpi <jimmy980189@gmail.com>
2022-10-13 09:11:00 +0800

    add testcase and compare shell script

--- podem/cmpAss1a.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-./atpg -logicsim -input ./input/c17.input ../circuits/iscas85/c17.bench | sed -n '4,+7p' > tmp/tmpA && cat output/c17.output | awk -F' ' '{print $2 " " $4}' > tmp/tmpB && diff tmp/tmpA tmp/tmpB && echo "c17.bench Ok"
-
-./atpg -logicsim -input ./input/c7552.input ../circuits/iscas85/c7552.bench | sed -n '4,+39p' > tmp/tmpA && cat output/c7552.output | awk -F' ' '{print $2 " " $4}' > tmp/tmpB && diff tmp/tmpA tmp/tmpB && echo "c7522.bench Ok"
---
+++ podem/cmpAss2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# For the first part
+./atpg -logicsim -input ./input/c17.input ../circuits/iscas85/c17.bench | sed -n '4,+7p' > tmp/tmpA && cat output/c17.output | awk -F' ' '{print $2 " " $4}' > tmp/tmpB && diff tmp/tmpA tmp/tmpB && echo "c17.bench Ok"
+
+./atpg -logicsim -input ./input/c7552.input ../circuits/iscas85/c7552.bench | sed -n '4,+39p' > tmp/tmpA && cat output/c7552.output | awk -F' ' '{print $2 " " $4}' > tmp/tmpB && diff tmp/tmpA tmp/tmpB && echo "c7522.bench Ok"
+
+# For the second part check logicsim and modified logicsim
+
+for f in input/c17*;
+do
+    echo "-logicsim"
+    /usr/bin/time -f "Average memory usage: %K \nMaximum memory usage: %M\n" ./atpg -logicsim -input $f ../circuits/iscas85/c17.bench > tmp/tmpA
+    grep CPU tmp/tmpA
+
+    echo "-mod_logicsim"
+    /usr/bin/time -f "Average memory usage: %K \nMaximum memory usage: %M\n" ./atpg -mod_logicsim -input $f ../circuits/iscas85/c17.bench > tmp/tmpB
+    grep CPU tmp/tmpB
+
+    diff tmp/tmpA tmp/tmpB | grep -v "c\|Run\|CP\|---" || echo "$f Ok"
+    echo "---"
+done
+
+for f in input/c7552*;
+do
+    echo "-logicsim"
+    /usr/bin/time -f "Average memory usage: %K \nMaximum memory usage: %M\n" ./atpg -logicsim -input $f ../circuits/iscas85/c7552.bench > tmp/tmpA
+    grep CPU tmp/tmpA
+
+    echo "-mod_logicsim"
+    /usr/bin/time -f "Average memory usage: %K \nMaximum memory usage: %M\n" ./atpg -mod_logicsim -input $f ../circuits/iscas85/c7552.bench > tmp/tmpB
+    grep CPU tmp/tmpB
+
+    diff tmp/tmpA tmp/tmpB | grep -v "c\|Run\|CP\|---" || echo "$f Ok"
+    echo "---"
+done
